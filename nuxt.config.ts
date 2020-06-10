ba14f688780c0d3340c1c7c1d1580ca4184e27c6
mnao305 <naomasa305@gmail.com>
2020-06-10 16:31:30 +0900

    chore: nuxt.configのTS化

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
-import colors from 'vuetify/es5/util/colors'
+import { Configuration } from '@nuxt/types'
 
-export default {
+const config: Configuration = {
   mode: 'universal',
   /*
   ** Headers of the page
@@ -51,21 +51,7 @@ export default {
   ** https://github.com/nuxt-community/vuetify-module
   */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+    customVariables: ['~/assets/variables.scss']
   },
   /*
   ** Build configuration
@@ -78,3 +64,5 @@ export default {
     }
   }
 }
+
+export default config
