002996ebf5bd28c9f4e01cbbfefe76c3dc9a8f50
mnao305 <naomasa305@gmail.com>
2020-06-10 22:38:52 +0900

    feat: 記事一覧を表示できた

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <nuxt-link to="/article/hoge">
-      articles ->
-    </nuxt-link>
+    <div v-for="article in articles" :key="article.path">
+      <nuxt-link :to="'article/'+article.slug">
+        {{ article.title }}
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent, useAsync, useContext } from 'nuxt-composition-api'
 
+export default defineComponent({
+  setup () {
+    const { $content } = useContext()
+    const articles = useAsync(async () => await $content('articles').limit(10).fetch())
+
+    return { articles }
+  }
+})
 </script>
