4a5820ef741b10e216bc12c248ab299143f9a93c
mnao305 <naomasa305@gmail.com>
2020-06-14 17:25:20 +0900

    feat: 雑にフッターを設置

--- client/components/organisms/Footer/index.vue
+++ client/components/organisms/Footer/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    フッターが入る
-  </div>
+  <v-footer>
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
 </template>
 
 <script lang="ts">
