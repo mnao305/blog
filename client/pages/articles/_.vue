ae2269315616bcbcec06e150e62419238284904f
mnao305 <naomasa305@gmail.com>
2020-06-14 18:40:39 +0900

    feat: 記事タイトルの表示

--- client/pages/articles/_.vue
+++ client/pages/articles/_.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1 class="text-h4">{{ post ? post.title : '' }}</h1>
     <nuxt-content :document="post" />
   </div>
 </template>
@@ -7,11 +8,21 @@
 <script lang="ts">
 import { defineComponent, useAsync, useContext } from 'nuxt-composition-api'
 
+type postT = {
+  title: string
+  description: string
+  path: string
+  tags: string[]
+  createdDate: string
+  updateDate?: string
+  body: Object
+}
+
 export default defineComponent({
   setup() {
     const { $content, route } = useContext()
     const post = useAsync(
-      async () => await $content(route.value.path.slice(1)).fetch()
+      async () => (await $content(route.value.path.slice(1)).fetch()) as postT
     )
 
     return { post }
