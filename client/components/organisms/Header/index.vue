006d5c72b104915f8732046bc1a95cbe714b6e50
mnao305 <naomasa305@gmail.com>
2020-06-14 18:17:44 +0900

    feat: ヘッダーを仮設置

--- client/components/organisms/Header/index.vue
+++ client/components/organisms/Header/index.vue
@@ -1,6 +1,9 @@
 <template>
   <header>
-    ここにヘッダーが入る
+    <h1 class="text-h3 d-flex justify-center">
+      <nuxt-link to="/">物置小屋</nuxt-link>
+    </h1>
+    <p class="text-subtitle-1 d-flex justify-center">メモ置き場</p>
   </header>
 </template>
 
@@ -11,3 +14,10 @@ export default defineComponent({
   setup() {},
 })
 </script>
+
+<style lang="scss" scoped>
+h1 > a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
