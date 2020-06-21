aeb1e555cee4ce7f443397635d9f0b58a6ebab67
mnao305 <naomasa305@gmail.com>
2020-06-21 21:42:12 +0900

    feat: ogp情報取得・追加済みのタグ生成スクリプトの追加

---
+++ scripts/linkCardGenerator.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+const https = require('https')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+
+const url = process.argv[2]
+
+const req = https.request(url, (res: any) => {
+  let html = ''
+  res.on('data', (chunk: any) => {
+    html += chunk
+  })
+  res.on('end', () => {
+    const ogpObj: any = {}
+    const dom = new JSDOM(html)
+
+    const headEls = dom.window.document.head.children
+    for (let i = 0; i < headEls.length; i++) {
+      const prop = headEls[i].getAttribute('property')
+
+      if (!prop) continue
+      ogpObj[prop.split(':')[1]] = headEls[i].getAttribute('content')
+    }
+    console.log(ogpObj)
+    console.log('↓----------------')
+    console.log(
+      `<link-card title="${ogpObj.site_name}" text="${ogpObj.description}" link-url="${ogpObj.url}" img-src="${ogpObj.image}"></link-card>`
+    )
+  })
+})
+req.end()
