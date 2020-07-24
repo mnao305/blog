0f5da9b4ae1169b4ca5a415bbe6ae90c50344e54
mnao305 <naomasa305@gmail.com>
2020-07-25 08:50:56 +0900

    feat: スポンサーボタンの設置

--- client/components/organisms/BottomContents/index.vue
+++ client/components/organisms/BottomContents/index.vue
@@ -1,15 +1,29 @@
 <template>
   <aside id="BottomContents">
-    <a target="_blank" href="https://github.com/mnao305">
-      <img
-        class="github-grass-img"
-        src="https://grass-graph.moshimo.works/images/mnao305.png"
-      />
-    </a>
-    <nuxt-link to="/about">
-      About(免責事項/Googleアナリティクスについて)
-    </nuxt-link>
-    <!-- BottomContents Twitterとかタグ一覧とか置く感じかな？ -->
+    <div>
+      <a target="_blank" href="https://github.com/mnao305">
+        <img
+          class="github-grass-img"
+          src="https://grass-graph.moshimo.works/images/mnao305.png"
+        />
+      </a>
+    </div>
+    <v-row>
+      <v-col cols="12" md="6">
+        <iframe
+          src="https://github.com/sponsors/mnao305/card"
+          title="Sponsor mnao305"
+          width="100%"
+          style="border: 0;"
+        ></iframe>
+      </v-col>
+      <v-col cols="12" md="6">
+        <nuxt-link to="/about">
+          About(免責事項/Googleアナリティクスについて)
+        </nuxt-link>
+      </v-col>
+      <!-- BottomContents Twitterとかタグ一覧とか置く感じかな？ -->
+    </v-row>
   </aside>
 </template>
 
