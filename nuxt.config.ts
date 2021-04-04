5692516435376d10b288c9e9881bb89d0822081d
mnao305 <mail@mnao305.com>
2021-04-04 10:09:36 +0900

    fix: リンクできなくなってた

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -117,7 +117,7 @@ const config: NuxtConfig = {
           feed.addItem({
             title: article.title,
             id: url,
-            link: url,
+            path: url,
             date: new Date(article.createdAt),
             description,
             content: description,
